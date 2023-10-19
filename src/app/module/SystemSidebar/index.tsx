e47f70eba45ed78e8e0619deea7a830fffcf47ad
Harish Iyer <harishiyer@Admins-MacBook-Pro.local>
2023-10-19 11:38:16 -0400

    planning dashboard

--- src/app/module/SystemSidebar/index.tsx
+++ src/app/module/SystemSidebar/index.tsx
@@ -3,6 +3,9 @@
 import Link from "next/link";
 import { useState } from "react";
 import { FaBarsStaggered } from "react-icons/fa6";
+import { IoIosSpeedometer } from "react-icons/io";
+import { BsFillCalendarRangeFill } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SystemSidebar = () => {
   const [showNavDrawer, setShowNavDrawer] = useState(false);
@@ -38,19 +41,9 @@ const SystemSidebar = () => {
           aria-controls="drawer-navigation"
           className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
         >
-          <svg
-            aria-hidden="true"
-            className="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
+          <span className="text-2xl">
+            <AiOutlineClose />
+          </span>
           <span className="sr-only">Close menu</span>
         </button>
         <div className="py-4 overflow-y-auto">
@@ -60,17 +53,11 @@ const SystemSidebar = () => {
                 href="/"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <svg
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 21"
-                >
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                </svg>
-                <span className="ml-3">Dashboard</span>
+                <span className="text-2xl">
+                  <IoIosSpeedometer />
+                </span>
+
+                <span className="ml-3">Pacing</span>
               </Link>
             </li>
 
@@ -79,15 +66,9 @@ const SystemSidebar = () => {
                 href="/plan"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 18 20"
-                >
-                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
-                </svg>
+                <span className="text-xl">
+                  <BsFillCalendarRangeFill />
+                </span>
                 <span className="flex-1 ml-3 whitespace-nowrap">Plan</span>
               </Link>
             </li>
@@ -100,7 +81,7 @@ const SystemSidebar = () => {
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="none" 
+                  fill="none"
                   viewBox="0 0 18 16"
                 >
                   <path
