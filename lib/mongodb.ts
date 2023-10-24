23c3f7cc5669a795ad9414387c8ae87bdaf26210
Harish Iyer <harishiyer@Admins-MacBook-Pro.local>
2023-10-24 16:21:30 -0400

    mongo db integration and changes

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 
 async function connectMongoDB() {
   try {
+    // Check if environment variable is accessible
+    if (!process.env.MONGODB_URI) {
+      console.log(
+        "%cERROR: DB ENVIRONMENT VARIABLE MIGHT NOT BE SET",
+        "font-weight: bold; color: red"
+      );
+    }
+
     // Connect the client to the server	(optional starting in v4.7)
     await mongoose.connect(process.env.MONGODB_URI);
     // Send a ping to confirm a successful connection
